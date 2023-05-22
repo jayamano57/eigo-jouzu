0e070d5459e6b576b9be54f703a43c5d69f53bdd
jay <jayamano57@gmail.com>
2023-05-22 16:26:03 -0700

    change button heigth width

--- src/components/MessageInput/MessageInput.tsx
+++ src/components/MessageInput/MessageInput.tsx
@@ -33,7 +33,8 @@ export function MessageInput({
       >
         {
           <svg
-            style={{ height: '100%', width: '100%' }}
+            height="24px"
+            width="24px"
             viewBox="0 0 24 24"
             fill="white"
             xmlns="http://www.w3.org/2000/svg"
