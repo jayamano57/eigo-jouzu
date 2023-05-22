c86641963f5132d52a7879431c1934eed90f05e9
jay <jayamano57@gmail.com>
2023-05-22 16:07:37 -0700

    update go button and prompt

--- src/services/AI/ai.service.ts
+++ src/services/AI/ai.service.ts
@@ -4,7 +4,7 @@ export class AIServiceImpl implements AIService {
   constructor(private baseUrl = 'https://api.openai.com/v1/') {}
 
   async completion(message: string, emojis?: false): Promise<unknown> {
-    const prompt = `Proofread the following text message to a friend. If there are any Japanese words or phrases in the message, translate them to English first. Fix all spelling errors and grammar mistakes. Do not put the outputted message in quotes.${
+    const prompt = `Proofread the following text message to a friend. If there are any Japanese words or phrases in the message, translate them to English first. Fix all spelling errors and grammar mistakes, and use friendly language. Do not put the outputted message in quotes.${
       emojis
         ? ' If it makes sense, you can one emoji in the message. The emoji can be either 😁, 💖, or 👍.'
         : ''
@@ -18,7 +18,7 @@ export class AIServiceImpl implements AIService {
       body: JSON.stringify({
         model: 'text-davinci-003',
         temperature: 0,
-        max_tokens: 256,
+        max_tokens: 1000,
         prompt,
       }),
     })
